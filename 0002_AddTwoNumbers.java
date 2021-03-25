338d80574aa284ccd72f18478a48c8bc8d295812
wendli <lwvoid@foxmail.com>
2021-03-25 11:19:14 +0800

    add: 0002_AddTwoNumbers

--- 0002_AddTwoNumbers.java
+++ 0002_AddTwoNumbers.java
@@ -10,22 +10,23 @@
  */
 class Solution {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode result = new ListNode(0, null);
+    ListNode result = new ListNode(0);
     ListNode curr = result;
 
     int c = 0, r = 0;
     while(l1 != null || l2 != null || c != 0) {
       int n1 = l1 == null ? 0 : l1.val;
       int n2 = l2 == null ? 0 : l2.val;
+      int sum = n1 + n2 + c;
 
-      r = (n1 + n2 + c) % 10;
-      c = (n1 + n2 + c) / 10;
+      r = sum % 10;
+      c = sum > 9 ? 1 : 0;
 
       curr.next = new ListNode(r);
+      curr = curr.next;
 
       if (l1 != null) l1 = l1.next;
       if (l2 != null) l2 = l2.next;
-      curr = curr.next;
     }
 
     return result.next;
