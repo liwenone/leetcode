1b911ee3825b5a45a3bf9aafacb8ce1f76a73af7
wendli <lwvoid@foxmail.com>
2021-03-26 10:58:41 +0800

    add: 0003_LongestSubstringWithoutRepeatingCharacters

--- 0003_LongestSubstringWithoutRepeatingCharacters.java
+++ 0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,17 +1,22 @@
 class Solution {
   public int lengthOfLongestSubstring(String s) {
-    if (s.length() == 0) return 0;
+    if (s.length() <= 1) {
+      return s.length();
+    }
 
-    int max = 0;
-    for (int i = 0; i < s.length() - 1; i++) {
-      for (int j = i + 1; j < s.length(); j++) {
-        String prefix = s.substring(i, j);
-        String curr = s.substring(j, j + 1);
-        if (prefix.contains(curr)) break;
-
-        int currMax = j - i;
-        if (currMax > max) max = currMax;
+    int max = 0, j = 0;
+    for (int i = 1; i < s.length(); i++) {
+      char c1 = s.charAt(i);
+      for (int k = j; k < i; k++) {
+        char c2 = s.charAt(k);
+        if (c1 == c2) {
+          j = k + 1;
+          break;
+        }
       }
+
+      int currMax = i - j;
+      if (currMax > max) max = currMax;
     }
 
     return max + 1;
