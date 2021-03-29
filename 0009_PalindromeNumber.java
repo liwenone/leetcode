db2853f5f208a76f3588d5959b1ac15b8b8449b1
wendli <lwvoid@foxmail.com>
2021-03-29 10:33:45 +0800

    add: 0009_PalindromeNumber

--- 0009_PalindromeNumber.java
+++ 0009_PalindromeNumber.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || x % 10 == 0 && x != 0) return false;
+
+        if (x < 10) return true;
 
         int tmp = x;
         int ans = 0;
-        while (x != 0) {
+        int count = 1;
+        while (x / 10 != 0) {
             ans = ans * 10 + (x % 10);
             x /= 10;
+            count *= 10;
         }
 
-        return ans == tmp;
+        return ans == tmp / 10 &&  ans / (count / 10) == x;
     }
 }
