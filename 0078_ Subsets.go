93bd850bdbbf0d2e629b9cd04b2937cb2e04b632
liwenone <liwenone@qq.com>
2021-05-28 11:20:56 +0800

    add: 0078_ Subsets

---
+++ 0078_ Subsets.go
@@ -0,0 +1,19 @@
+package main
+
+func subsets(nums []int) [][]int {
+	result := make([][]int, 0)
+	list := make([]int, 0)
+	backtrack(nums, 0, list, &result)
+	return result
+}
+
+func backtrack(nums []int, pos int, list []int, result *[][]int) {
+	ans := make([]int, len(list))
+	copy(ans, list)
+	*result = append(*result, ans)
+	for i := pos; i < len(nums); i++ {
+		list = append(list, nums[i])
+		backtrack(nums, i+1, list, result)
+		list = list[:len(list)-1]
+	}
+}
